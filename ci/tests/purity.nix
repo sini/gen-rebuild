25ca1348403fd12ad21b43bad0351162f65ed745
Jason Bowman <jason@json64.dev>
2026-07-15 19:56:24 -0700

    style: apply nixfmt (line-length reflow after gen bump)

--- ci/tests/purity.nix
+++ ci/tests/purity.nix
@@ -46,7 +46,8 @@ let
   ];
 
   violations = lib.concatMap (
-    src: map (tok: "${src.name}: '${tok}'") (lib.filter (tok: genPrelude.hasInfix tok src.code) forbidden)
+    src:
+    map (tok: "${src.name}: '${tok}'") (lib.filter (tok: genPrelude.hasInfix tok src.code) forbidden)
   ) sources;
 in
 {
